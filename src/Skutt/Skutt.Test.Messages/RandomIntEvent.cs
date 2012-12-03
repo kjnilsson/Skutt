8e884c50daf3e63434a92ceaa82bd9b9ee2233fe
Karl Nilsson <kjnilsson@gmail.com>
2012-12-03 17:26:34 +0000

    broadcaster

---
+++ src/Skutt/Skutt.Test.Messages/RandomIntEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skutt.Test.Messages
+{
+    public class RandomIntEvent
+    {
+        public int SomeNumber { get; set; }
+    }
+}
